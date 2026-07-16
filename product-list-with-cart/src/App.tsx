c9112ff9fa19e4b9be0849462edf44516b4ffcec
José Julio <86504989+josejulio1@users.noreply.github.com>
2026-07-16 09:19:16 +0200

    Updated product-list-with-cart project
    Removed duplicated CSS property and the dissapearing scroll issue has been fixed

--- product-list-with-cart/src/App.tsx
+++ product-list-with-cart/src/App.tsx
@@ -9,12 +9,16 @@ function App() {
     const { isOpen } = useModal();
 
     useEffect(() => {
-        document.body.classList.toggle('overflow-y-hidden');
+        if (isOpen) {
+            document.body.classList.add('overflow-y-hidden');
+        } else {
+            document.body.classList.remove('overflow-y-hidden');
+        }
     }, [isOpen]);
 
     return (
         <>
-            <main className="bg-rose-50 max-md:px-8 md:px-8 lg:px-10 xl:px-32 max-lg:py-8 lg:py-16 grid max-md:grid-cols-1 md:grid-cols-2 lg:grid-cols-[2fr_1fr] gap-8 font-[Red_Hat]">
+            <main className="bg-rose-50 max-md:px-8 md:px-8 lg:px-10 xl:px-32 max-lg:py-8 lg:py-16 grid max-md:grid-cols-1 md:grid-cols-2 lg:grid-cols-[2fr_1fr] gap-8">
                 <section className="flex flex-col gap-y-10">
                     <h1 className="text-5xl font-bold text-rose-900">Desserts</h1>
                     <ProductsGrid />
