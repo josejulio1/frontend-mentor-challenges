1cb663858587e1f494d03ef43ddd80515ae37685
José Julio <86504989+josejulio1@users.noreply.github.com>
2026-07-16 09:13:28 +0200

    Updated product-list-with-cart project
    Wrapped products grid in a component

--- product-list-with-cart/src/App.tsx
+++ product-list-with-cart/src/App.tsx
@@ -1,10 +1,9 @@
 import { useEffect } from "react";
 import Cart from "./components/cart/Cart";
 import OrderConfirmedModal from "./components/order-confirmed-modal/OrderConfirmedModal";
-import ProductCard from "./components/product-card/ProductCard";
-import products from "./data/data.json";
 import useModal from "./stores/modal.store";
 import { AnimatePresence } from "framer-motion";
+import ProductsGrid from "./components/products-grid/ProductsGrid";
 
 function App() {
     const { isOpen } = useModal();
@@ -18,16 +17,7 @@ function App() {
             <main className="bg-rose-50 max-md:px-8 md:px-8 lg:px-10 xl:px-32 max-lg:py-8 lg:py-16 grid max-md:grid-cols-1 md:grid-cols-2 lg:grid-cols-[2fr_1fr] gap-8 font-[Red_Hat]">
                 <section className="flex flex-col gap-y-10">
                     <h1 className="text-5xl font-bold text-rose-900">Desserts</h1>
-                    <section className="grid max-md:grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 gap-8">
-                        {
-                            products.map(product => (
-                                <ProductCard
-                                    key={product.name}
-                                    product={product}
-                                />
-                            ))
-                        }
-                    </section>
+                    <ProductsGrid />
                 </section>
                 <Cart />
             </main>
---
+++ product-list-with-cart/src/components/products-grid/ProductsGrid.tsx
@@ -0,0 +1,19 @@
+import ProductCard from "../product-card/ProductCard";
+import products from "./../../data/data.json";
+
+function ProductsGrid() {
+    return (
+        <section className="grid max-md:grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 gap-8">
+            {
+                products.map(product => (
+                    <ProductCard
+                        key={product.name}
+                        product={product}
+                    />
+                ))
+            }
+        </section>
+    );
+}
+
+export default ProductsGrid;
